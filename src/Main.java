1e30fda7d5c58339ed93e1c67764c30893486eb2
Danylo Baziewicz <danxbazi@gmail.com>
2025-04-12 11:14:51 -0300

    Adicionado teste de get e set de Serie.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
-import br.com.alura.screenmatch.models.Movie.Movie;
+import br.com.alura.screenmatch.models.Movie;
+import br.com.alura.screenmatch.models.Serie;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,10 +8,22 @@ public class Main {
         myMovie.setYearOfRelease(2011);
         myMovie.setLengthInMinutes(120);
 
-        myMovie.printMovieInfo();
-        System.out.println(myMovie.averageRating());
-        System.out.println("Total number of ratings: " + myMovie.getNumberOfRatings());
+//      myMovie.printMovieInfo();
+//      System.out.println(myMovie.averageRating());
+//      System.out.println("Total number of ratings: " + myMovie.getNumberOfRatings());
 
+        Serie mySerie = new Serie();
+        mySerie.setTitle("Game of Thrones");
+        mySerie.setYearOfRelease(2011);
+        mySerie.setLengthInMinutes(120);
+        mySerie.setNumberOfSeasons(5);
+        mySerie.setNumberOfEpisodes(12);
+        mySerie.setActive(true);
+        mySerie.setMinutesPerEpisode(15);
+
+        mySerie.printMovieInfo();
+        System.out.println(mySerie.averageRating());
+        System.out.println("Total number of ratings: " + mySerie.getNumberOfRatings());
     }
 
 
