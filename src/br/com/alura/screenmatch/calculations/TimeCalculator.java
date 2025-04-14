da0ca84cb4aa3ad0e7fe5d4b8dedb803de9086d3
Danylo Baziewicz <danxbazi@gmail.com>
2025-04-14 20:48:16 -0300

    Criada classe TimeCalculator para calcular tempo total de Movies e Serie.

--- src/br/com/alura/screenmatch/calculations/TimeCalculator.java
+++ src/br/com/alura/screenmatch/calculations/TimeCalculator.java
@@ -1,4 +1,24 @@
 package br.com.alura.screenmatch.calculations;
 
+import br.com.alura.screenmatch.models.Movie;
+import br.com.alura.screenmatch.models.Title;
+
 public class TimeCalculator {
+    private int totalOfTime;
+
+    public int getTotalOfTime() {
+        return totalOfTime;
+    }
+
+//    public void adds(Movie m) {
+//        totalOfTime += m.getLengthInMinutes();
+//    }
+
+    public void adds(Title t) {
+        this.totalOfTime += t.getLengthInMinutes();
+    }
+
+
+
+
 }
