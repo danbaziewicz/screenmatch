c9d3061c18adc23ca0a3afbc8d4a69380bbf7278
Danylo Baziewicz <danxbazi@gmail.com>
2025-04-14 20:49:12 -0300

    Feita a implementação de geração de média de avaliação através do Rating.

--- src/br/com/alura/screenmatch/models/Movie.java
+++ src/br/com/alura/screenmatch/models/Movie.java
@@ -1,6 +1,8 @@
 package br.com.alura.screenmatch.models;
 
-public class Movie extends Title{
+import br.com.alura.screenmatch.calculations.Rating;
+
+public class Movie extends Title implements Rating {
     private String director;
 
     public String getDirector() {
@@ -9,5 +11,10 @@ public class Movie extends Title{
 
     public void setDirector(String director) {
         this.director = director;
+    }
+
+    @Override
+    public int getRating() {
+        return (int) (averageRating() / 2);
     }
 }
