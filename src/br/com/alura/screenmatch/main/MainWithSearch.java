8efca4fe6198176a564279e9716a6d9e8b62fd13
Danylo Baziewicz <danxbazi@gmail.com>
2025-04-24 21:06:07 -0300

    refactor: aplica URLEncoder ao input do usuário para evitar erros na URL da API

--- src/br/com/alura/screenmatch/main/MainWithSearch.java
+++ src/br/com/alura/screenmatch/main/MainWithSearch.java
@@ -2,16 +2,24 @@ package br.com.alura.screenmatch.main;
 
 import java.io.IOException;
 import java.net.URI;
+import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 public class MainWithSearch {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite o filme que deseja pesquisar: ");
+        var search = URLEncoder.encode(scan.nextLine(), StandardCharsets.UTF_8);
+
+        String url = "https://www.omdbapi.com/?t=" + search + "&apikey=65fc0c38";
 
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=spirited_away&apikey=65fc0c38"))
+                .uri(URI.create(url))
                 .build();
 
         HttpResponse<String> response = client
